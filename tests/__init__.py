ba1cc6befc10aa02e98f0fbe86ad3422e9b2279d
enottweak1A <alinakesh87@gmail.com>
2025-08-31 10:18:38 +0300

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,17 @@
-from src.mymodule import add
+from setuptools import setup, find_packages
 
-def test_add():
-  assert add(2, 3) == 5
+setup(
+    name="MyPythonApp",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "requests>=2.31",
+        "numpy>=1.26"
+    ],
+    entry_points={
+        "console_scripts": [
+            "myapp=src.mymodule:main",
+        ],
+    },
+)
